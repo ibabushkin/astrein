c2eee1efcbb39ee920d5c198ecd834c6c67f054b
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-07-12 22:43:11 +0200

    Added a first draft of typeclass-based parsing of queries

--- src/Language/ASTrein/AST.hs
+++ src/Language/ASTrein/AST.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 module Language.ASTrein.AST where
 
+import Control.Applicative ((<|>))
+
+import Data.Attoparsec.Text
 import Data.Text (Text)
 
 -- | a typeclass associating a type for a language-specific AST with
@@ -10,5 +13,31 @@ class AST a where
     data Query a :: *
     -- | apply query
     match :: a -> Query a -> a
+    -- | all parsers needed to parse a `Text` into a `Query a`
+    parsers :: Parsers a
     -- | parse a query from text
     parseQuery :: Text -> Maybe (Query a)
+
+data Parsers a = Parsers
+    { elements :: [Parser (Query a)]
+    , chains :: [Parser (Query a)]
+    }
+
+-- | generate a parser for an atomic query for a named object, for instance
+-- a type, class or value.
+elementParser :: AST a
+              => Char
+              -> (Text -> Query a)
+              -> Parser (Query a)
+elementParser c cons = char c *> (cons <$> name)
+    where name = takeWhile1 (`notElem` (" ()" :: String))
+
+lineNumParser :: AST a => (Integer -> Query a) -> Parser (Query a)
+lineNumParser = (<$> decimal)
+
+chainingParser :: AST a
+               => Text
+               -> (Query a -> Query a -> Query a)
+               -> Parser (Query a)
+chainingParser sep cons = cons <$> toplevel <*> (string sep *> toplevel)
+    where toplevel = choice $ (mappend <$> chains <*> elements) parsers
