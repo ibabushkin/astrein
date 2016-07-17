eaa4d511f3c42ba0aad24cd65335956fff628bb7
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-07-17 19:28:25 +0200

    Reworked AST implementation for Haskell

--- src/Language/ASTrein/AST/Haskell.hs
+++ src/Language/ASTrein/AST/Haskell.hs
@@ -3,29 +3,40 @@ module Language.ASTrein.AST.Haskell where
 
 import Language.ASTrein.AST
 import Language.ASTrein.AST.Template
-import Language.Haskell.Exts.Annotated
+import qualified Language.Haskell.Exts.Annotated as H
 
 import Data.Text (Text)
 
 -- | a Haskell AST
 data HaskellAST = HaskellAST
-    { moduleHead :: Maybe (ModuleHead SrcSpanInfo)
-    , modulePragmas :: [ModulePragma SrcSpanInfo]
-    , imports :: [ImportDecl SrcSpanInfo]
-    , decls :: [Decl SrcSpanInfo]
+    { moduleHead :: Maybe (H.ModuleHead H.SrcSpanInfo)
+    , modulePragmas :: [H.ModulePragma H.SrcSpanInfo]
+    , imports :: [H.ImportDecl H.SrcSpanInfo]
+    , decls :: [H.Decl H.SrcSpanInfo]
     } deriving (Show, Eq)
 
-data ModuleHeadQuery
-    = MHNameQuery String
-    | MHExportQuery ExportSpecQuery
+-- | query a module name
+newtype NQuery = NQuery Text
 
-data ExportSpecQuery = ESName String
+-- | query an export declaration
+newtype EQuery = EQuery Text
 
-data ImportQuery = IName String
+-- | query an import declaration
+newtype IQuery = IQuery Text
 
-data DeclQuery
-    = DTypeName String
-    | DTypeSig () -- ^ TODO
-    | DClassDecl () -- ^ TODO
-    | DInstanceDecl () -- ^ TODO
-    | FunBind () -- ^ TODO
+data DQuery
+    = TypeName Text -- ^ query for a type's origin - this might be a data decl
+    | TypeFamily Text -- ^ query for a type family - this might be a data fam
+    | Class Text -- ^ query for a typeclass
+    | Instance Text Text -- ^ query for some instance
+    | TypeSignature () -- TODO: implement
+    | Func Text -- ^ query for a function
+
+instance AST HaskellAST where
+    data Query HaskellAST
+        = MName NQuery
+        | EName EQuery
+        | IName IQuery
+        | DName DQuery
+    match a _ = a
+    parsers = Parsers [] []
